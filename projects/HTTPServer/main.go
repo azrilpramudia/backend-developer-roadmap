70f311dc0030ed15d980020caebe3c76f207d1ce
azrilpramudia <azrilpramudia01@gmail.com>
2026-08-25 14:09:06 +0700

    add URL Parameter

--- projects/HTTPServer/main.go
+++ projects/HTTPServer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"log/slog"
@@ -10,20 +11,19 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", handleHello)
+	mux.HandleFunc("/", handleRoot)
 	mux.HandleFunc("/goodbye", handleGoodbye)
+	mux.HandleFunc("/hello/", handleHelloParameterized)
 
 	log.Fatal(http.ListenAndServe(":3000", mux))
 }
 
-func handleHello(w http.ResponseWriter, _ *http.Request) {
-	wc, err := w.Write([]byte("Hello, World!\n"))
+func handleRoot(w http.ResponseWriter, _ *http.Request) {
+	_, err := w.Write([]byte("Welcome to our Server\n"))
 	if err != nil {
 		slog.Error("error writing response", "err", err)
 		return
 	}
-
-	fmt.Printf("%d bytes written\n", wc)
 }
 
 func handleGoodbye(w http.ResponseWriter, _ *http.Request) {
@@ -34,4 +34,25 @@ func handleGoodbye(w http.ResponseWriter, _ *http.Request) {
 	}
 
 	fmt.Printf("%d bytes written\n", wc)
+}
+
+func handleHelloParameterized(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	userList := params["user"]
+
+	username := "User"
+	if len(userList) > 0 {
+		username = userList[0]
+	}
+
+	var output bytes.Buffer
+	output.WriteString("Hello, ")
+	output.WriteString(username)
+	output.WriteString("!\n")
+
+	_, err := w.Write(output.Bytes())
+	if err != nil {
+		slog.Error("error writing response body", "err", err)
+		return
+	}
 }
--- projects/HTTPServer/main_test.go
+++ projects/HTTPServer/main_test.go
@@ -7,11 +7,11 @@ import (
 	"testing"
 )
 
-func TestHandleHello(t *testing.T) {
+func TestHandleRoot(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/hello", nil)
 	w := httptest.NewRecorder()
 
-	handleHello(w, req)
+	handleRoot(w, req)
 
 	desiredCode := http.StatusOK
 	if w.Code != desiredCode {
@@ -19,7 +19,7 @@ func TestHandleHello(t *testing.T) {
 	desiredCode, w.Code, w.Body.String())
 	}
 
-	expectedMessage := []byte("Hello, World!\n")
+	expectedMessage := []byte("Welcome to our Server\n")
 	if !bytes.Equal(expectedMessage, w.Body.Bytes()) {
 		t.Errorf("bad return, got: %q, expected %q", w.Body.String(), expectedMessage)
 	}
@@ -43,3 +43,56 @@ func TestHandleGoodbye(t *testing.T) {
 	}
 }
 
+func TestHandleHelloParameterized(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/hello?user=TestMan", nil) 
+	w := httptest.NewRecorder()
+
+	handleHelloParameterized(w, req)
+
+	desiredCode := http.StatusOK
+	if w.Code != desiredCode {
+		t.Errorf("bad responese code, expected %v but got %v\nBody: %s\n",
+	desiredCode, w.Code, w.Body.String())
+	}
+
+	expectedMessage := []byte("Hello, TestMan!\n")
+	if !bytes.Equal(expectedMessage, w.Body.Bytes()) {
+		t.Errorf("bad return, got: %q, expected %q", w.Body.String(), expectedMessage)
+	}
+}
+
+func TestHandleHelloParameterizedNoParam(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/hello/", nil) 
+	w := httptest.NewRecorder()
+
+	handleHelloParameterized(w, req)
+
+	desiredCode := http.StatusOK
+	if w.Code != desiredCode {
+		t.Errorf("bad responese code, expected %v but got %v\nBody: %s\n",
+	desiredCode, w.Code, w.Body.String())
+	}
+
+	expectedMessage := []byte("Hello, User!\n")
+	if !bytes.Equal(expectedMessage, w.Body.Bytes()) {
+		t.Errorf("bad return, got: %q, expected %q", w.Body.String(), expectedMessage)
+	}
+}
+
+func TestHandleHelloParameterizedWrongParam(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/hello?foo=bar", nil) 
+	w := httptest.NewRecorder()
+
+	handleHelloParameterized(w, req)
+
+	desiredCode := http.StatusOK
+	if w.Code != desiredCode {
+		t.Errorf("bad responese code, expected %v but got %v\nBody: %s\n",
+	desiredCode, w.Code, w.Body.String())
+	}
+
+	expectedMessage := []byte("Hello, User!\n")
+	if !bytes.Equal(expectedMessage, w.Body.Bytes()) {
+		t.Errorf("bad return, got: %q, expected %q", w.Body.String(), expectedMessage)
+	}
+}
