80a9f5963671a16569ef86dc1b624fbaf01dfd39
azrilpramudia <azrilpramudia01@gmail.com>
2026-09-02 00:39:49 +0700

    simulating data storage

---
+++ projects/HTTPServer/internal/users/users.go
@@ -0,0 +1,69 @@
+package users
+
+import (
+	"errors"
+	"fmt"
+	"net/mail"
+)
+
+var ErrNoResultFound = errors.New("no results found")
+
+type User struct {
+	FirstName string
+	LastName string
+	Email mail.Address
+}
+
+type Manager struct {
+	users []User
+}
+
+func NewManager() *Manager {
+	return &Manager{}
+}
+
+func (m *Manager) AddUser(firstName string, lastName string, email string) error {
+	if firstName == "" {
+		return fmt.Errorf("invalid first name: %q", firstName)
+	}
+
+	if lastName == "" {
+		return fmt.Errorf("invalid last name: %q", lastName)
+	}
+
+	existingUser, err := m.GetUserByName(firstName, lastName)
+	if err != nil && !errors.Is(err, ErrNoResultFound){
+		return fmt.Errorf("error checking if user is already present: %v", err)
+	}
+
+	if existingUser != nil {
+		return errors.New("user with this name already exists")
+	}
+	
+	parseAddress, err := mail.ParseAddress(email)
+	if err != nil {
+		return fmt.Errorf("invalid email: %s", email)
+	}
+
+	newUser := User{
+		FirstName: firstName,
+		LastName: lastName,
+		Email: *parseAddress,
+	}
+
+	m.users = append(m.users, newUser)
+	return nil
+}
+
+func (m *Manager) GetUserByName(first string, last string) (*User, error) {
+	for i, user := range m.users{
+		if user.FirstName == first && user.LastName == last {
+			// fmt.Printf("address in list: %p\n", &m.users[i])
+			result := m.users[i]
+			// fmt.Printf("address of new var: %p\n", &result)
+			return &result, nil
+		}
+	}
+
+	return nil, ErrNoResultFound
+}
